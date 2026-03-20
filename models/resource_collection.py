da3e93b7329f3a263fe56967c608ee9cc04ed98d
TrupleCPU <janamaya438@gmail.com>
2026-03-21 02:00:23 +0800

    update: resource_collection model

--- models/resource_collection.py
+++ models/resource_collection.py
@@ -1,11 +1,12 @@
 from . import db
 from sqlalchemy.sql import func
+from sqlalchemy.dialects.postgresql import JSONB
 
 class ResourceCollection(db.Model):
     __tablename__ = 'resource_collection'
     collection_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
-    description = db.Column(db.Text, nullable=True)
+    description = db.Column(JSONB, nullable=True)
     is_published = db.Column(db.Boolean, default=False, nullable=False)
     created_at = db.Column(db.DateTime(timezone=True), server_default=func.now(), nullable=False)
     updated_at = db.Column(db.DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
